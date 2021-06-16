000a29828883b04dcc9b5e5dafe47c3468237606
Alex Bezerra <51725824+OpsAlex@users.noreply.github.com>
2021-06-15 21:22:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using LogicaPagamentosJuros.Entities;
+using LogicaPagamentosJuros.Services;
+
+
+namespace LogicaPagamentosJuros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter contract data");
+                Console.Write("Number: ");
+                int contractNumber = int.Parse(Console.ReadLine());
+                Console.Write("Date (dd/MM/yyyy): ");
+                DateTime contractDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                Console.Write("Contract value: ");
+                double contractValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Enter number of installments: ");
+                int months = int.Parse(Console.ReadLine());
+
+
+                Contrato myContract = new Contrato(contractNumber, contractDate, contractValue);
+
+                ContratoDeService contractService = new ContratoDeService(new ParcelService());
+                contractService.ProcessContract(myContract, months);
+
+                Console.WriteLine("Installments:");
+                foreach (Parcelas installment in myContract.Parcelas)
+                {
+                    Console.WriteLine(installment);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
